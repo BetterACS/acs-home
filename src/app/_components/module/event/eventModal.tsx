db3abf4ee4598a4693b1983f9f1479b89d1020e2
monshinawatra <monshinawatra@loolootech.com>
2024-02-22 19:37:01 +0700

    Add components in modal

--- src/app/_components/module/event/eventModal.tsx
+++ src/app/_components/module/event/eventModal.tsx
@@ -1,13 +1,22 @@
+import { Input } from '@/components/ui/input';
 import { motion } from 'framer-motion';
 import Backdrop from '../../utils/backdrop';
+import DatePicker from '../../utils/datepicker';
+import type { TagItem } from '@/types';
+import EventModalInput, { SelfExpandTextarea, TagInput } from './eventModalInput';
 
-const EventModal = ({ handleClose, text }: any) => {
+const example_items: TagItem[] = [
+	{ value: '1', label: 'One' },
+	{ value: '2', label: 'Two' },
+	{ value: '3', label: 'Three' },
+];
+
+const EventModal = ({ handleClose }: any) => {
 	return (
 		<Backdrop onClick={handleClose}>
 			<motion.div
 				onClick={(e) => e.stopPropagation()}
-				className="rounded-[34px]"
-				style={{ width: '968px', height: '688px', background: '#E8E4E4' }}
+				className="rounded-[34px] bg-white w-[968px] h-[688px]"
 				initial={{ opacity: 0 }}
 				animate={{ opacity: 1 }}
 				exit={{ opacity: 0 }}
@@ -19,14 +28,32 @@ const EventModal = ({ handleClose, text }: any) => {
 					<div className="m-4 rounded-full w-[48px] h-[48px] bg-gray-200" onClick={handleClose}></div>
 				</div>
 
-				<div style={{ overflowY: 'auto', maxHeight: '606px' }}>
-					<div
-						className="mt-[78px] ml-[95px] w-[747px] h-[78px] rounded-[33px]"
-						style={{ background: '#BEBEBE' }}
-					></div>
-				</div>
+				<div className="flex flex-col items-center h-full" style={{ overflowY: 'auto', maxHeight: '560px' }}>
+					<Input
+						className="w-[747px] h-[78px] mt-[78px] text-4xl focus-visible:ring-0 focus-visible:ring-ring focus-visible:ring-offset-0 border-0"
+						style={{ background: '#E8E4E4' }}
+						placeholder="Unititled"
+						autoFocus
+					/>
 
-				<p>{text}</p>
+					<div className="self-start w-[480px] ml-[124px]">
+						<EventModalInput label="Due date" marginTop={30} width={240} textSize="lg">
+							<DatePicker />
+						</EventModalInput>
+
+						<EventModalInput label="Coin" marginTop={20} width={240} textSize="lg">
+							<Input className="w-[200px]" placeholder="100+" />
+						</EventModalInput>
+
+						<EventModalInput label="Type" marginTop={20} width={240} textSize="lg">
+							<TagInput message="Select item" items={example_items} />
+						</EventModalInput>
+					</div>
+
+					<div className="w-[747px] mt-[40px] h-full">
+						<SelfExpandTextarea />
+					</div>
+				</div>
 			</motion.div>
 		</Backdrop>
 	);
