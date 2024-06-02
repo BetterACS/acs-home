74cccfcc5795a1a85ca723e4f39200cea2500ed7
monshinawatra <monshinawatra@loolootech.com>
2024-06-02 12:51:12 +0700

    Add searching features && Add search box animation

--- src/components/module/event/searchBox.tsx
+++ src/components/module/event/searchBox.tsx
@@ -13,6 +13,31 @@ interface MultiFunctionButtonProps {
 	onClick?: () => void;
 }
 
+import { PlaceholdersAndVanishInput } from '@/components/ui/placeholder';
+
+export function PlaceholdersAndVanishInputDemo() {
+	const placeholders = [
+		'Help me with my homework',
+		'I need help with my project',
+		'How to install a package in python?',
+	];
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		console.log('Typing in search box', e.target.value);
+	};
+	const onSubmit = (value: String) => {
+		console.log('submitted', value);
+	};
+	return (
+		<div className="flex flex-row items-center space-x-4 w-full">
+			<div className="rounded-full bg-blue-500 w-12 h-12 flex flex-col items-center justify-center">
+				<Image src={'/search-white.gif'} alt="search-icon" width={30} height={30} className="bg-transparent" />
+			</div>
+			<PlaceholdersAndVanishInput placeholders={placeholders} onChange={handleChange} onSubmit={onSubmit} />
+		</div>
+	);
+}
+
 function MultiFunctionButton(props: MultiFunctionButtonProps) {
 	const { children, width, icon, onClick } = props;
 	return (
@@ -41,30 +66,8 @@ export default function SearchBox(props: any) {
 				</div>
 				<Meteors className="bg-white" />
 			</button>
-
-			{/* <MultiFunctionButton width={477} onClick={setModalOpen}>
-				<p>Create</p>
-			</MultiFunctionButton> */}
-			{/* <MultiFunctionButton width={685}>
-				<p>Search</p>
-			</MultiFunctionButton> */}
-			<div className="w-[685px] rounded-full bg-gray-200 p-2 flex flex-row items-center ">
-				<div className="rounded-full bg-blue-500 w-12 h-12 flex flex-col items-center justify-center">
-					{/* <Search size={26} color="white" /> */}
-					{/* <img src={logo} alt="" /> */}
-					<Image
-						src={'/search-white.gif'}
-						alt="search-icon"
-						width={30}
-						height={30}
-						className="bg-transparent"
-					/>
-				</div>
-				<input
-					type="text"
-					className="ml-2 w-[580px] h-full text-2xl bg-gray-200 focus:outline-0"
-					placeholder="Search"
-				/>
+			<div className="w-[685px] rounded-full bg-white p-2 flex flex-row items-center shadow-md">
+				<PlaceholdersAndVanishInputDemo />
 			</div>
 		</motion.div>
 	);
