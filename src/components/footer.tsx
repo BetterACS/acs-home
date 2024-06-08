33953aac0a0fad6fdd31043cb461d3d90806a1cc
monshinawatra <monshinawatra@loolootech.com>
2024-06-09 00:18:12 +0700

    Implement More UI

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,27 @@
 
 export default function Footer() {
 	return (
-		<>
-			<a href="https://lordicon.com/">Icons by Lordicon.com</a>
-		</>
+		// <div className="flex flex-row justify-center">
+		<div className="custom-shape-divider-top-1717865832">
+			<svg
+				width="140%"
+				height="142px"
+				fill="#ffffff"
+				className="rotate-180"
+				data-name="Layer 1"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 1000 120"
+				preserveAspectRatio="none"
+			>
+				<path
+					d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+					className="shape-fill"
+				></path>
+			</svg>
+			<div className="bg-white flex flex-row justify-center py-4 space-x-6">
+				<a href="https://lordicon.com/">Icons by Lordicon.com</a>
+				<a href="https://www.shapedivider.app/">Curve by shapedivider</a>
+			</div>
+		</div>
 	);
 }
